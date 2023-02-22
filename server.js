6dcb97425fb43de2c760006f3443df2d7bbad0a0
Mark Green <mrgssg@gmail.com>
2023-02-22 15:24:59 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const routes = require('.routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+
+// adding helper function
+const helpers = require('./utils/helpers');
+
+// sequelize
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 // middleware 
 app.use(express.json());
