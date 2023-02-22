541c5c68fba7ba326c7e0f60ca8ae5b8e1d662d4
Mark Green <mrgssg@gmail.com>
2023-02-22 15:07:34 -0500

    started updating user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,59 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['[password]']}
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    });
+
+    router.get('/id:', (req, res) => {
+        User.findOne({
+            attributes: { exclude: ['password'] },
+            where: {
+                id: req.params.id
+            },
+            include: [
+                {
+                    model: Post,
+                    attributes: [
+                        'id',
+                        'title',
+                        'content',
+                        'create_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'create_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                },
+                {
+                    model: Post,
+                    attributes: ['title'],
+                }
+            ]
+        })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'no user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+
+
+// 'Post' api users
+
+})
