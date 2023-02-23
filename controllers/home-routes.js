5a624cdac5e824a0f17d922675158786dacd2741
Mark Green Jr <mrgssg@gmail.com>
2023-02-23 14:41:33 -0500

    updated home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -94,3 +94,47 @@ router.get('/post/:id', (req, res) => {
 });
 
 // redirecting user to see their posts 
+router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'create_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'create_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']     
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'no post found with this id' });
+            return;
+        }
+
+        // sequelize this data
+        const post = dbPostData.get({ plain: true });
+
+        res.render('posts-comments', {post, loggedIn: req.session.loggedIn});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
