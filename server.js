6803d4dd78396abfc405dd9d7e0c179689d773cf
Mark Green <mrgssg@gmail.com>
2023-02-24 14:28:26 -0500

    added more middleware to server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
+const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 // adding helper function
 const helpers = require('./utils/helpers');
@@ -15,16 +18,25 @@ const session = require('express-session');
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {maxAge: 10000},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
 
 
 // middleware 
 app.use(express.json());
+app.use(session(sess));
 app.use(express.urlencoded({ extended: true}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes)
 
 // access db and server with sequelize
